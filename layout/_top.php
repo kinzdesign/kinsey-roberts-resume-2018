d024563d23a5f8d5b1a44de0e0c830c53e634d69
Kinsey <kinzdesign@gmail.com>
2017-07-22 11:11:42 -0400

    Added container wrapper div

--- layout/_top.php
+++ layout/_top.php
@@ -7,3 +7,4 @@
 <?# Google Tag Manager (noscript) ?>
     <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WHZM4LT" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <? require($_SERVER['DOCUMENT_ROOT'] . '/layout/_topnav.php'); ?>
+    <div class="container">
