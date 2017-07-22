921a2e8716e8895969611dece92e57e5f233dd2a
Kinsey <kinzdesign@gmail.com>
2017-07-22 11:08:39 -0400

    Split into partials

---
+++ layout/_head.php
@@ -0,0 +1,15 @@
+<?# These three meta tags *must* come first in the head; any other head content must come *after* these tags ?>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<?# Latest compiled and minified CSS ?>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<?# HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries ?>
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<?# Page title ?>
+    <title><?php 
+      echo 'Kinsey Roberts' 
+    ?></title>
---
+++ layout/_top.php
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+<? require($_SERVER['DOCUMENT_ROOT'] . '/layout/_head.php'); ?>
+  </head>
+  <body>
