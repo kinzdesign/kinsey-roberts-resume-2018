d482e31770013b1be57d7fbe56ce4d043cd5590a
Kinsey <kinzdesign@gmail.com>
2017-07-22 11:23:34 -0400

    Installed FontAwesome via CDN

--- layout/_head.php
+++ layout/_head.php
@@ -4,6 +4,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 <?# Google Tag Manager ?>
     <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-WHZM4LT');</script>
+<?# font-awesome CDN ?>
+    <script src="https://use.fontawesome.com/ced7440677.js"></script>
 <?# Latest compiled and minified CSS ?>
     <link rel="stylesheet" href="/assets/css/resume.min.css">
 <?# HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries ?>
