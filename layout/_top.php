6b72d2b760bf29cb75d29f516c3a3d44eb523117
Kinsey <kinzdesign@gmail.com>
2017-07-22 11:54:37 -0400

    Made topnavbar fixed

--- layout/_top.php
+++ layout/_top.php
@@ -5,7 +5,7 @@
   <head>
 <? require($_SERVER['DOCUMENT_ROOT'] . '/layout/_head.php'); ?>
   </head>
-  <body>
+  <body class="top-navbar-fixed">
 <?# Google Tag Manager (noscript) ?>
     <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WHZM4LT" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <? require($_SERVER['DOCUMENT_ROOT'] . '/layout/_topnav.php'); ?>
