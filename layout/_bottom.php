322ddc0079513ddda5728441f88786f5976a02c3
Kinsey <kinzdesign@gmail.com>
2017-07-22 11:37:05 -0400

    Added local jQuery fallback

--- layout/_bottom.php
+++ layout/_bottom.php
@@ -1,6 +1,7 @@
     </div>
-<?# jQuery (necessary for Bootstrap's JavaScript plugins) ?>
+<?# jQuery (necessary for Bootstrap's JavaScript plugins), with local fallback ?>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="/assets/js/vendor/jquery.min.js"><\/script>')</script>
 <?# Latest compiled and minified JavaScript ?>
     <script src="/assets/js/vendor/bootstrap.min.js"></script>
   </body>
