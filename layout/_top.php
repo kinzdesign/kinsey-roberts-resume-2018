12c63a56099a07e080b3efc131c302a92b00df86
Kinsey <kinzdesign@gmail.com>
2017-07-22 11:09:26 -0400

    Installed Google Tag Manager

--- layout/_top.php
+++ layout/_top.php
@@ -4,3 +4,5 @@
 <? require($_SERVER['DOCUMENT_ROOT'] . '/layout/_head.php'); ?>
   </head>
   <body>
+<?# Google Tag Manager (noscript) ?>
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WHZM4LT" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
