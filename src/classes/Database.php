8df920a4234577ed74f15b98fb09dc1defc8809f
Kinsey <kinzdesign@gmail.com>
2017-07-23 16:46:02 -0400

    Fleshed out Database class to include static functions for common tasks

--- src/classes/Database.php
+++ src/classes/Database.php
@@ -1,24 +1,40 @@
 <?php
 class Database {
 
-  // reuse a single connection
-  static $conn;
+  public static function getConnection() { 
+    // create connection
+    $db = newAdoConnection('mysqli');
+    // fetch associated arrays by default
+    $db->setFetchMode(ADODB_FETCH_ASSOC);
+    // connect using environment variables
+    if(!$db->connect(Config::dbHost(), Config::dbUser(), Config::dbPass(), Config::dbDatabase()))
+      return false;
+    // return connected connection object
+    return $db;
+  }
 
-  public static function getConnection($debug = false) { 
-    // connect if connection not configured
-    if(!self::$conn) {
-      // create connection
-      self::$conn = newAdoConnection('mysqli');
-      // fetch associated arrays by default
-      self::$conn->setFetchMode(ADODB_FETCH_ASSOC);
-      // connect using environment variables
-      if(!self::$conn->connect(Config::dbHost(), Config::dbUser(), Config::dbPass(), Config::dbDatabase()))
-        return false;
-      // return connected connection object
-    }
-    // set debug flag
-    self::$conn->debug = $debug;
-    return self::$conn;
+  public static function execute($sql, $inputarr = false) {
+    return self::getConnection()->execute($sql, $inputarr);
+  }
+
+  public static function getAll($sql, $inputarr = false) {
+    return self::getConnection()->getAll($sql, $inputarr);
+  }
+
+  public static function getAssoc($sql, $inputarr = false, $force_array = false, $first2cols = false) {
+    return self::getConnection()->getAssoc($sql, $inputarr, $force_array, $first2cols);
+  }
+
+  public static function getCol($sql, $inputarr = false, $trim = false) {
+    return self::getConnection()->getCol($sql, $inputarr, $trim);
+  }
+
+  public static function getOne($sql, $inputarr = false) {
+    return self::getConnection()->getOne($sql, $inputarr);
+  }
+
+  public static function getRow($sql, $inputarr = false) {
+    return self::getConnection()->getRow($sql, $inputarr);
   }
 
 }
