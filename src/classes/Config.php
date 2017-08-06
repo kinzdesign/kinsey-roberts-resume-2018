a7b975d89b9a4227da094397831896b63964bf95
Kinsey <kinzdesign@gmail.com>
2017-08-06 12:09:38 -0400

    Removed unused Bootstrap from custom Sass

--- src/classes/Config.php
+++ src/classes/Config.php
@@ -2,7 +2,7 @@
 class Config {
 
   // timestamp of last build
-  public static function getBuildTime() { return '1502035079'; }
+  public static function getBuildTime() { return '1502035688'; }
   public static function echoBuildTime() { echo self::getBuildTime(); }
 
   // get database configuration variables
