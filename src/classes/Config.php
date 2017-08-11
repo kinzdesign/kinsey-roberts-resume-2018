edea97f339685a5641df68428c0e456c36ced4a9
Kinsey <kinzdesign@gmail.com>
2017-08-11 18:59:55 -0400

    Refactored topnav Sass files

--- src/classes/Config.php
+++ src/classes/Config.php
@@ -2,7 +2,7 @@
 class Config {
 
   // timestamp of last build
-  public static function getBuildTime() { return '1502491964'; }
+  public static function getBuildTime() { return '1502492380'; }
   public static function echoBuildTime() { echo self::getBuildTime(); }
 
   // get database configuration variables
