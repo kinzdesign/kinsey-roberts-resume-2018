e35516e224208fb07479901e89df09a0e5a85885
Kinsey <kinzdesign@gmail.com>
2017-08-20 14:15:25 -0400

    Added fa-fw class to Home icon in breadcrumbs

--- src/partials/layout/_breadcrumbs.php
+++ src/partials/layout/_breadcrumbs.php
@@ -2,7 +2,7 @@
 <?php 
   foreach(Page::$breadcrumbs as $name => $url)
     if($name == 'Home')
-      echo "            <li><a href=\"$url\" data-category=\"Breadcrumb\" data-action=\"Click - Home\" title=\"Home\"><i class=\"fa fa-lg fa-home\" title=\"Home\"></i></a></li>\n";
+      echo "            <li><a href=\"$url\" data-category=\"Breadcrumb\" data-action=\"Click - Home\" title=\"Home\"><i class=\"fa fa-lg fa-fw fa-home\" title=\"Home\"></i></a></li>\n";
     else
       echo "            <li><a href=\"$url\" data-category=\"Breadcrumb\" data-action=\"Click - {$name}\">$name</a></li>\n";
 ?>
