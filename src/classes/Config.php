80ecbdab69bdb37f19b3a74e00bd661c1cd2f29d
Kinsey <kinzdesign@gmail.com>
2017-08-20 14:11:17 -0400

    Use home icon in breadcrumbs

--- src/classes/Config.php
+++ src/classes/Config.php
@@ -2,7 +2,7 @@
 class Config {
 
   // timestamp of last build
-  public static function getBuildTime() { return '1503252365'; }
+  public static function getBuildTime() { return '1503252651'; }
   public static function echoBuildTime() { echo self::getBuildTime(); }
 
   // get database configuration variables
--- src/partials/layout/_breadcrumbs.php
+++ src/partials/layout/_breadcrumbs.php
@@ -1,6 +1,9 @@
           <ol class="breadcrumb">
 <?php 
   foreach(Page::$breadcrumbs as $name => $url)
-    echo "            <li><a href=\"$url\" data-category=\"Breadcrumb\" data-action=\"Click - {$name}\">$name</a></li>\n";
+    if($name == 'Home')
+      echo "            <li><a href=\"$url\" data-category=\"Breadcrumb\" data-action=\"Click - Home\" title=\"Home\"><i class=\"fa fa-lg fa-home\" title=\"Home\"></i></a></li>\n";
+    else
+      echo "            <li><a href=\"$url\" data-category=\"Breadcrumb\" data-action=\"Click - {$name}\">$name</a></li>\n";
 ?>
           </ol>
