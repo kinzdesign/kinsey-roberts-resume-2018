5636cc5655788a0c8c87d0b0cfe443f039efda27
Kinsey <kinzdesign@gmail.com>
2017-08-20 14:36:28 -0400

    Fixed typo in scripts partial

--- src/partials/layout/_scripts.php
+++ src/partials/layout/_scripts.php
@@ -1,6 +1,6 @@
 <?php 
   // jQuery (necessary for Bootstrap's JavaScript plugins), with local fallback 
-  if(Page::$jsJquery || Page::$jsResume) {
+  if(Page::$jsJQuery || Page::$jsResume) {
   ?>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="/assets/js/vendor/jquery.min.js"><\/script>')</script>
