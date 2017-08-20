e4dfd73d280f7fe319fa83c224e435ceb9aee4ee
Kinsey <kinzdesign@gmail.com>
2017-08-20 12:33:07 -0400

    Refactored Tenure to use duration_string

--- src/classes/Tenure.php
+++ src/classes/Tenure.php
@@ -107,17 +107,7 @@ class Tenure {
   }
 
   public function duration() {
-    if($this->months < 3)
-      return '';
-    $y = intdiv($this->months, 12);
-    $yLabel = $y != 1 ? 'years' : 'year';
-    $m = $this->months % 12;
-    $mLabel = $m != 1 ? 'months' : 'month';
-    if($m == 0)
-      return "$y $yLabel";
-    if($y == 0)
-      return "$m $mLabel";
-    return "$y $yLabel, $m $mLabel";
+    return duration_string($this->months());
   }
 
   public function bullets() {
