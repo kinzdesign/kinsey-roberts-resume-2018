aab5fd0e2f4f3b856f5b9e85a8dfd4fb006dbd0f
Kinsey <kinzdesign@gmail.com>
2017-09-01 18:53:52 -0400

    Added schema.org markup to project page

--- src/classes/Config.php
+++ src/classes/Config.php
@@ -2,7 +2,7 @@
 class Config {
 
   // timestamp of last build
-  public static function getBuildTime() { return '1504304247'; }
+  public static function getBuildTime() { return '1504306395'; }
   public static function echoBuildTime() { echo self::getBuildTime(); }
 
   // get database configuration variables
