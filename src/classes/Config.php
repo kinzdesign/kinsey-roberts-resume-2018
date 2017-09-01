d895bab1d075c477dbb35536332177fec2495194
Kinsey <kinzdesign@gmail.com>
2017-08-31 21:00:45 -0400

    Added markup for name and email

--- src/classes/Config.php
+++ src/classes/Config.php
@@ -2,7 +2,7 @@
 class Config {
 
   // timestamp of last build
-  public static function getBuildTime() { return '1504224886'; }
+  public static function getBuildTime() { return '1504227597'; }
   public static function echoBuildTime() { echo self::getBuildTime(); }
 
   // get database configuration variables
