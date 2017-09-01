b33fafbe7d3e5ff9b99d87a86f103faf5ce7dd56
Kinsey <kinzdesign@gmail.com>
2017-09-01 18:54:38 -0400

    Added schema.org markup to breadcrumbs

--- src/classes/Config.php
+++ src/classes/Config.php
@@ -2,7 +2,7 @@
 class Config {
 
   // timestamp of last build
-  public static function getBuildTime() { return '1504306395'; }
+  public static function getBuildTime() { return '1504306455'; }
   public static function echoBuildTime() { echo self::getBuildTime(); }
 
   // get database configuration variables
