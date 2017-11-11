14bdaa63c1b81611c490fc4882c91b18ae1b7082
Kinsey <kinzdesign@gmail.com>
2017-11-10 23:06:29 -0500

    Inlined gtm.js

--- src/partials/layout/_head.php
+++ src/partials/layout/_head.php
@@ -3,7 +3,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 <?php # Google Tag Manager with DNT honoring ?>
-    <script src="/assets/js/gtm<?php echo (Page::isStatic() ? '.min' : '') . '.js?ts=' . Config::getBuildTime(); ?>"></script>
+    <script><?php echo(file_get_contents(getcwd().'\assets\js\gtm.min.js')); ?></script>
 <?php # Latest compiled and minified CSS ?>
     <link rel="stylesheet" href="/assets/css/<?php echo Page::$cssFile; ?>.min.css?ts=<?php Config::echoBuildTime() ?>">
 <?php # HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries ?>
