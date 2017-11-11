41132ef3b2fdf59ad421cb69fa7a8a15940a9e3f
Kinsey <kinzdesign@gmail.com>
2017-11-11 16:13:17 -0500

    Updated s3_sync to set content-type with charset on text-based files

--- tasks/s3_sync.php
+++ tasks/s3_sync.php
@@ -31,13 +31,25 @@ function sync_file($root, $fileinfo) {
       break;
     # compress text-based formats
     case 'html':
+      gzip_file($root, $fileinfo->getPathname(), '--content-type text/html;charset=utf-8');
+      break;
     case 'js':
+      gzip_file($root, $fileinfo->getPathname(), '--content-type text/javascript;charset=utf-8');
+      break;
     case 'css':
+      gzip_file($root, $fileinfo->getPathname(), '--content-type text/css;charset=utf-8');
+      break;
     case 'xml':
+      gzip_file($root, $fileinfo->getPathname(), '--content-type text/xml;charset=utf-8');
+      break;
     case 'svg':
+      gzip_file($root, $fileinfo->getPathname(), '--content-type image/svg+xml;charset=utf-8');
+      break;
     case 'json':
+      gzip_file($root, $fileinfo->getPathname(), '--content-type application/json;charset=utf-8');
+      break;
     case 'txt':
-      gzip_file($root, $fileinfo->getPathname());
+      gzip_file($root, $fileinfo->getPathname(), '--content-type text/plain;charset=utf-8');
       break;
     # upload binary files as-is
     default:
@@ -50,12 +62,12 @@ function relative_path($root, $pathname) {
   return str_replace('\\','/',str_replace($root, '', $pathname));
 }
 
-function s3_upload($pathname, $relative, $encoding = '') {
-  $cmd = "aws s3 cp --cache-control max-age=2592000 --expires 2034-01-01T00:00:00Z {$encoding} $pathname s3://kinseyroberts.me{$relative}";
+function s3_upload($pathname, $relative, $args = '') {
+  $cmd = "aws s3 cp --cache-control max-age=2592000 --expires 2034-01-01T00:00:00Z {$args} $pathname s3://kinseyroberts.me{$relative}";
   echo str_replace('upload:', '   > uploaded:', exec($cmd)) . "\r\n";
 }
 
-function gzip_file($root, $pathname) {
+function gzip_file($root, $pathname, $args = "") {
   // build gz file path
   $gzippath = "$pathname.gz";
   // open the gz file (w9 = highest compression)
@@ -67,7 +79,7 @@ function gzip_file($root, $pathname) {
   // get relative path
   $relative = relative_path($root, $pathname);
   // upload gz file
-  s3_upload($gzippath, $relative, '--content-encoding gzip');
+  s3_upload($gzippath, $relative, '--content-encoding gzip ' . $args);
   // delete gz file
   unlink($gzippath);
 }
@@ -84,4 +96,5 @@ $root = realpath("../static");
 sync_dir($root, $root);
 // ping Google with new sitemap
 $ping = 'https://www.google.com/webmasters/tools/ping?sitemap=' . urlencode(Config::productionHost() . '/sitemap.xml');
-echo '   > pinging Google at ' . $ping;
+echo "   > pinging Google at $ping\r\n";
+file_get_contents($ping);
