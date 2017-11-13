83cd35e1d3e0d896f1904b0f621522fb0c63b775
Kinsey <kinzdesign@gmail.com>
2017-11-12 20:38:38 -0500

    Add noscript link tag as fallback

--- src/partials/layout/_head.php
+++ src/partials/layout/_head.php
@@ -8,6 +8,8 @@
   if(Page::$cssFile)
     echo '<link rel="stylesheet" href="/assets/css/'.Page::$cssFile.'.min.css'.Page::cacheBreaker(true).'"/>';
 ?>
+<?php # Fallback font loading if JavaScript is disabled ?>
+<noscript><link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:700|Open+Sans"></noscript>
 <?php # HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries ?>
 <!--[if lt IE 9]><script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script><script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
 <?php # Page title ?>
