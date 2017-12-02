dad46695c720247d83f1aee491c599da377e6fbe
Kinsey <kinzdesign@gmail.com>
2017-12-02 17:59:26 -0500

    Condensed compile_assets scripts

--- tasks/compile_assets.rb
+++ tasks/compile_assets.rb
@@ -1,3 +1,26 @@
 Dir.chdir("/")
-require_relative '_compile_assets'
-compile_assets()
+require 'git'
+require_relative '_compile_sass'
+require_relative '_concat_javascript'
+require_relative '_get_webroot'
+require_relative '_update_timestamp'
+
+puts
+# compute webroot and sass-cache directory
+webroot = get_webroot()
+sass_cache_dir = File.join(webroot, ".sass-cache")
+
+# open Git repository
+git = Git.open(webroot)
+
+# compile sass
+compile_sass(webroot, "resume", sass_cache_dir, git)
+compile_sass(webroot, "pdf-viewer", sass_cache_dir, git)
+
+# compile javascript
+concat_javascript(webroot, "resume", git)
+concat_javascript(webroot, "inline", git)
+
+# update timestamp to expire caches
+update_timestamp(webroot, git)
+puts
