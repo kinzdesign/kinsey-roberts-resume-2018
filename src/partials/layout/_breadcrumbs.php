efc2cb1020c0da0f205170f5f4f47f65408dd71a
Kinsey <kinzdesign@gmail.com>
2017-12-03 11:27:20 -0500

    Moved breadcrumbs outside main

--- src/partials/layout/_breadcrumbs.php
+++ src/partials/layout/_breadcrumbs.php
@@ -1,19 +1,19 @@
-          <ol class="breadcrumb" itemscope itemtype="http://schema.org/BreadcrumbList">
+      <ol class="breadcrumb" itemscope itemtype="http://schema.org/BreadcrumbList">
 <?php 
   $i = 0;
   foreach(Page::$breadcrumbs as $name => $url) {
     $i++;
-    echo "            <li itemprop=\"itemListElement\" itemscope itemtype=\"http://schema.org/ListItem\">\r\n";
-    echo "              <meta itemprop=\"position\" content=\"{$i}\" />\r\n";
-    echo "              <a href=\"$url\" data-category=\"Breadcrumb\" data-action=\"Click - {$name}\" itemscope itemtype=\"http://schema.org/Thing\" itemprop=\"item\">\r\n";
+    echo "        <li itemprop=\"itemListElement\" itemscope itemtype=\"http://schema.org/ListItem\">\r\n";
+    echo "          <meta itemprop=\"position\" content=\"{$i}\" />\r\n";
+    echo "          <a href=\"$url\" data-category=\"Breadcrumb\" data-action=\"Click - {$name}\" itemscope itemtype=\"http://schema.org/Thing\" itemprop=\"item\">\r\n";
     if($name == 'Home') {
-      echo "                <i class=\"fa fa-lg fa-fw fa-home\" aria-hidden=\"true\"></i>\r\n";
-      echo "                <span itemprop=\"name\" class=\"hidden\">$name</span>\r\n";
+      echo "            <i class=\"fa fa-lg fa-fw fa-home\" aria-hidden=\"true\"></i>\r\n";
+      echo "            <span itemprop=\"name\" class=\"hidden\">$name</span>\r\n";
     } else {
-      echo "                <span itemprop=\"name\">$name</span>\r\n";
+      echo "            <span itemprop=\"name\">$name</span>\r\n";
     }
-    echo "              </a>\r\n";
-    echo "            </li>\r\n";
+    echo "          </a>\r\n";
+    echo "        </li>\r\n";
   }
 ?>
-          </ol>
+      </ol>
